aaff6d3e4d08509814d3006025ab02fd49315202
Yongjik Kim <yongjik.kim2@gmail.com>
2023-04-25 22:45:40 +0900

    Initial commit for GraphRec model

---
+++ GraphRec/GraphRec.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import pickle
+import numpy as np
+from model import GraphRec, UV_Aggregator, UV_Encoder, Social_Aggregator, Social_Encoder
+import torch.utils.data
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from math import sqrt
+import argparse
+import os
+
+"""
+GraphRec: Graph Neural Networks for Social Recommendation. 
+Wenqi Fan, Yao Ma, Qing Li, Yuan He, Eric Zhao, Jiliang Tang, and Dawei Yin. 
+In Proceedings of the 28th International Conference on World Wide Web (WWW), 2019. Preprint[https://arxiv.org/abs/1902.07243]
+
+If you use this code, please cite our paper:
+```
+@inproceedings{fan2019graph,
+  title={Graph Neural Networks for Social Recommendation},
+  author={Fan, Wenqi and Ma, Yao and Li, Qing and He, Yuan and Zhao, Eric and Tang, Jiliang and Yin, Dawei},
+  booktitle={WWW},
+  year={2019}
+}
+```
+"""
+
+def train(model, device, train_loader, optimizer, epoch, best_rmse, best_mae):
+    model.train()
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        batch_nodes_u, batch_nodes_v, labels_list = data
+        optimizer.zero_grad()
+        loss = model.loss(batch_nodes_u.to(device), batch_nodes_v.to(device), labels_list.to(device))
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 100 == 0:
+            print('[%d, %5d] loss: %.3f, The best rmse/mae: %.6f / %.6f' % (
+                epoch, i, running_loss / 100, best_rmse, best_mae))
+            running_loss = 0.0
+    return 0
+
+
+def test(model, device, test_loader):
+    model.eval()
+    tmp_pred = []
+    target = []
+    with torch.no_grad():
+        for test_u, test_v, tmp_target in test_loader:
+            test_u, test_v, tmp_target = test_u.to(device), test_v.to(device), tmp_target.to(device)
+            val_output = model.forward(test_u, test_v)
+            tmp_pred.append(list(val_output.data.cpu().numpy()))
+            target.append(list(tmp_target.data.cpu().numpy()))
+    tmp_pred = np.array(sum(tmp_pred, []))
+    target = np.array(sum(target, []))
+    expected_rmse = sqrt(mean_squared_error(tmp_pred, target))
+    mae = mean_absolute_error(tmp_pred, target)
+    return expected_rmse, mae
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='Social Recommendation: GraphRec model')
+    parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size for training')
+    parser.add_argument('--embed_dim', type=int, default=64, metavar='N', help='embedding size')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR', help='learning rate')
+    parser.add_argument('--test_batch_size', type=int, default=1000, metavar='N', help='input batch size for testing')
+    parser.add_argument('--epochs', type=int, default=100, metavar='N', help='number of epochs to train')
+    args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    use_cuda = False
+    if torch.cuda.is_available():
+        use_cuda = True
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    embed_dim = args.embed_dim
+    dir_data = './data/toy_dataset'
+
+    path_data = dir_data + ".pickle"
+    data_file = open(path_data, 'rb')
+    history_u_lists, history_ur_lists, history_v_lists, history_vr_lists, train_u, train_v, train_r, test_u, test_v, test_r, social_adj_lists, ratings_list = pickle.load(
+        data_file)
+    """
+    ## toy dataset 
+    history_u_lists, history_ur_lists:  user's purchased history (item set in training set), and his/her rating score (dict)
+    history_v_lists, history_vr_lists:  user set (in training set) who have interacted with the item, and rating score (dict)
+    
+    train_u, train_v, train_r: training_set (user, item, rating)
+    test_u, test_v, test_r: testing set (user, item, rating)
+    
+    # please add the validation set
+    
+    social_adj_lists: user's connected neighborhoods
+    ratings_list: rating value from 0.5 to 4.0 (8 opinion embeddings)
+    """
+
+    trainset = torch.utils.data.TensorDataset(torch.LongTensor(train_u), torch.LongTensor(train_v),
+                                              torch.FloatTensor(train_r))
+    testset = torch.utils.data.TensorDataset(torch.LongTensor(test_u), torch.LongTensor(test_v),
+                                             torch.FloatTensor(test_r))
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=True)
+    num_users = history_u_lists.__len__()
+    num_items = history_v_lists.__len__()
+    num_ratings = ratings_list.__len__()
+
+    u2e = nn.Embedding(num_users, embed_dim).to(device)
+    v2e = nn.Embedding(num_items, embed_dim).to(device)
+    r2e = nn.Embedding(num_ratings, embed_dim).to(device)
+
+    # user feature
+    # features: item * rating
+    agg_u_history = UV_Aggregator(v2e, r2e, u2e, embed_dim, cuda=device, uv=True)
+    enc_u_history = UV_Encoder(u2e, embed_dim, history_u_lists, history_ur_lists, agg_u_history, cuda=device, uv=True)
+    # neighobrs
+    agg_u_social = Social_Aggregator(lambda nodes: enc_u_history(nodes).t(), u2e, embed_dim, cuda=device)
+    enc_u = Social_Encoder(lambda nodes: enc_u_history(nodes).t(), embed_dim, social_adj_lists, agg_u_social,
+                           base_model=enc_u_history, cuda=device)
+
+    # item feature: user * rating
+    agg_v_history = UV_Aggregator(v2e, r2e, u2e, embed_dim, cuda=device, uv=False)
+    enc_v_history = UV_Encoder(v2e, embed_dim, history_v_lists, history_vr_lists, agg_v_history, cuda=device, uv=False)
+
+    # model
+    graphrec = GraphRec(enc_u, enc_v_history, r2e).to(device)
+    optimizer = torch.optim.RMSprop(graphrec.parameters(), lr=args.lr, alpha=0.9)
+
+    best_rmse = 9999.0
+    best_mae = 9999.0
+    endure_count = 0
+
+    for epoch in range(1, args.epochs + 1):
+
+        train(graphrec, device, train_loader, optimizer, epoch, best_rmse, best_mae)
+        expected_rmse, mae = test(graphrec, device, test_loader)
+        # please add the validation set to tune the hyper-parameters based on your datasets.
+
+        # early stopping (no validation set in toy dataset)
+        if best_rmse > expected_rmse:
+            best_rmse = expected_rmse
+            best_mae = mae
+            endure_count = 0
+        else:
+            endure_count += 1
+        print("rmse: %.4f, mae:%.4f " % (expected_rmse, mae))
+
+        if endure_count > 5:
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ GraphRec/README.md
@@ -0,0 +1,13 @@
+# GraphRec: Graph Neural Networks for Social Recommendation
+
+This project is to a slight modification of the official implementation of the paper:
+
+[**<u>Wenqi Fan</u>**] (https://wenqifan03.github.io), Yao Ma , Qing Li, Yuan He, Eric Zhao, Jiliang Tang, and Dawei Yin. [Graph Neural Networks for Social Recommendation](https://arxiv.org/pdf/1902.07243.pdf). 
+In Proceedings of the 28th International Conference on World Wide Web (WWW), 2019. 
+Preprint[https://arxiv.org/abs/1902.07243]
+
+## TODO
+
+- Refactor the code for model extension
+- Train and test the model on datasets other than Epinions and Ciao
+- Test PyTorch 2.0 `.compile()` feature
